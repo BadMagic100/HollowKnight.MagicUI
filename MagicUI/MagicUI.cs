770a427dcba11e53f0d04fa8b73ff9d1222a0cf0
Sean Dempsey <dempsey.sean@outlook.com>
2022-01-05 18:53:34 -0800

    Add project files.

---
+++ MagicUI/MagicUI.cs
@@ -0,0 +1,28 @@
+﻿using Modding;
+
+namespace MagicUI
+{
+    public class MagicUI : Mod
+    {
+        internal static MagicUI? Instance { get; private set; }
+
+        public override string GetVersion()
+        {
+            return GetType().Assembly.GetName().Version.ToString();
+        }
+
+        public MagicUI() : base()
+        {
+            Instance = this;
+        }
+
+        public override void Initialize()
+        {
+            Log("Initializing");
+
+            //todo: make the magic happen (honestly may not even need stuff here lmao)
+
+            Log("Initialized");
+        }
+    }
+}
