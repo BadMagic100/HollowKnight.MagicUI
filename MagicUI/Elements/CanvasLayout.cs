c924456cb65ee0e87241eb41b6c600dc055fa48c
Sean Dempsey <dempsey.sean@outlook.com>
2022-03-05 21:21:39 -0800

    Add canvas layout, make debug bounds togglable

---
+++ MagicUI/Elements/CanvasLayout.cs
@@ -0,0 +1,67 @@
+﻿using MagicUI.Core;
+using UnityEngine;
+
+namespace MagicUI.Elements
+{
+    /// <summary>
+    /// A layout that places elements absolutely in screen space.
+    /// </summary>
+    /// <remarks>
+    /// Usually this is not a very idiomatic way to use MagicUI, but is helpful in the rare cases where you just need to move things around by hand.
+    /// One particularly good use case for this is to ease migrations for people who have existing CanvasUtil UIs and want to migrate to create
+    /// more complex layouts. <br/><br/>
+    /// This layout has some unusual properties to be aware of compared to other elements:
+    /// <ul>
+    /// <li>Because this layout will request to fill the entire screen, it may create unexpected results if it is set as the child of another element, and changing
+    /// its alignment and padding may result in children being placed off-screen</li>
+    /// <li>The alignment of children will not have any effect, as it will be arranged at its offset with exactly its effective size</li>
+    /// <li>The right and bottom paddings of children will not have any effect. It's recommended to use <see cref="XOffset"/> and <see cref="YOffset"/>
+    /// attached properties instead of padding to position children.</li>
+    /// </ul>
+    /// </remarks>
+    public class CanvasLayout : Layout
+    {
+        /// <summary>
+        /// The child's X offset in the canvas
+        /// </summary>
+        public static AttachedProperty<float> XOffset = new(0, ChangeAction.Arrange);
+        /// <summary>
+        /// The child's Y offset in the canvas
+        /// </summary>
+        public static AttachedProperty<float> YOffset = new(0, ChangeAction.Arrange);
+
+        /// <summary>
+        /// Creates a canvas layout
+        /// </summary>
+        /// <param name="onLayout">The layout root to draw the stack layout on</param>
+        /// <param name="name">The name of the stack layout</param>
+        public CanvasLayout(LayoutRoot onLayout, string name = "New CanvasLayout") : base(onLayout, name) { }
+
+        /// <inheritdoc/>
+        protected override Vector2 MeasureOverride()
+        {
+            foreach (ArrangableElement element in Children)
+            {
+                element.Measure();
+            }
+            return UI.Screen.size;
+        }
+
+        /// <inheritdoc/>
+        protected override void ArrangeOverride(Vector2 alignedTopLeftCorner)
+        {
+            foreach (ArrangableElement element in Children)
+            {
+                Vector2 pos = alignedTopLeftCorner + new Vector2(XOffset.Get(element), YOffset.Get(element));
+                Vector2 size = element.EffectiveSize;
+                element.Arrange(new Rect(pos, size));
+            }
+        }
+
+        /// <inheritdoc/>
+        protected override void DestroyOverride()
+        {
+            Children.Clear();
+        }
+    }
+}
--- MagicUIExamples/ExampleMod.cs
+++ MagicUIExamples/ExampleMod.cs
@@ -1,15 +1,28 @@
 ﻿using MagicUI.Core;
 using Modding;
+using System.Collections.Generic;
 
 namespace MagicUIExamples
 {
-    public class MagicUIExamples : Mod, ITogglableMod
+    public class MagicUIExamples : Mod, ITogglableMod, IMenuMod
     {
         internal static MagicUIExamples? Instance;
 
         public override string GetVersion() => GetType().Assembly.GetName().Version.ToString();
 
+        public bool ToggleButtonInsideMenu => true;
+
         private LayoutRoot? layout;
+
+        private bool debugBoundStatePersistent = false;
+        private void SetDebugBoundState(bool value)
+        {
+            debugBoundStatePersistent = value;
+            if (layout != null)
+            {
+                layout.RenderDebugLayoutBounds = value;
+            }
+        }
 
         public override void Initialize()
         {
@@ -27,7 +40,7 @@ namespace MagicUIExamples
             if (layout == null)
             {
                 layout = new(true, "Persistent layout");
-                layout.RenderDebugLayoutBounds = true;
+                layout.RenderDebugLayoutBounds = debugBoundStatePersistent;
 
                 // comment out examples as needed to see only the specific ones you want
                 SimpleLayoutExample.Setup(layout);
@@ -44,6 +57,24 @@ namespace MagicUIExamples
             On.HeroController.Awake -= OnSaveOpened;
             layout?.Destroy();
             layout = null;
+        }
+
+        public List<IMenuMod.MenuEntry> GetMenuData(IMenuMod.MenuEntry? toggleButtonEntry)
+        {
+            List<IMenuMod.MenuEntry> items = new()
+            {
+                new IMenuMod.MenuEntry {
+                    Name = "Show Debug Bounds",
+                    Values = new string[] {"On", "Off"},
+                    Saver = opt => SetDebugBoundState(opt == 0),
+                    Loader = () => debugBoundStatePersistent ? 0 : 1
+                }
+            };
+            if (toggleButtonEntry != null)
+            {
+                items.Insert(0, (IMenuMod.MenuEntry)toggleButtonEntry);
+            }
+            return items;
         }
     }
 }
